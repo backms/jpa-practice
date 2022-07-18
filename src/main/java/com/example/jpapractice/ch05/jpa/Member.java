42a32fa25c4977ac1805119090d320a148099243
백민수 <iphonex@kakao.com>
2022-07-18 23:24:37 +0900

    N:1 매핑

--- src/main/java/com/example/jpapractice/ch05/jpa/Member.java
+++ src/main/java/com/example/jpapractice/ch05/jpa/Member.java
@@ -2,14 +2,24 @@ package com.example.jpapractice.ch05.jpa;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
+@Entity
 @Data
 public class Member {
 
+    @Id
+    @Column(name = "MEMBER_ID")
     private String id;
+
     private String username;
 
+    // 연관관계 매핑
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
     private Team team;
 
+    // 연관관계 설정
     public void setTeam(Team team){
         this.team = team;
     }
--- src/main/java/com/example/jpapractice/ch05/jpa/Team.java
+++ src/main/java/com/example/jpapractice/ch05/jpa/Team.java
@@ -2,10 +2,18 @@ package com.example.jpapractice.ch05.jpa;
 
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 @Data
 public class Team {
 
+    @Id
+    @Column(name = "TEAM_ID")
     private String id;
+
     private String name;
 
 }
